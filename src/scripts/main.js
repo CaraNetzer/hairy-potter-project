e11a1e48b3d9b49899e6128e1e3f0952a7d4b559
Cara Netzer <cara.netzer@gmail.com>
2022-06-24 14:31:00 -0400

    added firePottery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,19 @@
 //import { makePottery } from "./PotteryWheel";
+const { firePottery } = require("./Kiln.js");
 const {makePottery} = require("./PotteryWheel.js")
 
-const pot1 = makePottery("round","5 lbs", "7 inches");
-const pot2 = makePottery("oval","6 lbs", "9 inches");
-const pot3 = makePottery("square","4 lbs", "5 inches");
-const pot4 = makePottery("rectangle","3 lbs", "6 inches");
-const pot5 = makePottery("diamond","5.5 lbs", "8 inches");
+let pot1 = makePottery("round","5 lbs", "7 inches");
+let pot2 = makePottery("oval","6 lbs", "9 inches");
+let pot3 = makePottery("square","4 lbs", "5 inches");
+let pot4 = makePottery("rectangle","3 lbs", "6 inches");
+let pot5 = makePottery("diamond","5.5 lbs", "8 inches");
+
+pot1 = firePottery(pot1, 2400);
+pot2 = firePottery(pot2, 2100);
+pot3 = firePottery(pot3, 2200);
+pot4 = firePottery(pot4, 2000);
+pot5 = firePottery(pot5, 2150);
+
 
 console.log(pot1);
 console.log(pot2);
